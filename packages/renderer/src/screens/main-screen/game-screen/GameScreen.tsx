f45775d99da33afb40ae9644c3222356a588c934
Zenoxs <amaury.civier@gmail.com>
2022-07-06 17:30:57 +0200

    force focus on active game

--- packages/renderer/src/screens/main-screen/game-screen/GameScreen.tsx
+++ packages/renderer/src/screens/main-screen/game-screen/GameScreen.tsx
@@ -3,7 +3,8 @@ import { useGameContext } from '@/providers'
 import { useStores } from '@/store'
 import { Game } from '@/store/game-store/game'
 import { useI18nContext } from '@lindo/i18n'
-import React, { memo, useRef } from 'react'
+import { reaction } from 'mobx'
+import React, { memo, useEffect, useRef } from 'react'
 import { manageGameWindow } from './manage-game-window'
 
 export interface GameScreenProps {
@@ -16,6 +17,21 @@ export const GameScreen = memo(({ game }: GameScreenProps) => {
   const rootStore = useStores()
   const { LL } = useI18nContext()
   const iframeGameRef = useRef<HTMLIFrameElementWithDofus>(null)
+
+  useEffect(() => {
+    return reaction(
+      () => rootStore.gameStore.selectedGame,
+      (selectedGame) => {
+        if (selectedGame?.id === game.id) {
+          console.log('force focus')
+          setTimeout(() => {
+            iframeGameRef.current?.focus()
+          }, 100)
+        }
+      },
+      { fireImmediately: true }
+    )
+  })
 
   const handleLoad = () => {
     if (iframeGameRef.current) {
