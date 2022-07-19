b21823aad64c52a2b6c9576665f031fd1bdfe515
zenoxs <amaury.civier@gmail.com>
2022-07-19 11:53:45 +0200

    improve the update

--- packages/main/updater/game-updater.ts
+++ packages/main/updater/game-updater.ts
@@ -39,6 +39,7 @@ export class GameUpdater {
     this._rootStore = rootStore
     this._httpClient = axios.create()
     this._dofusOrigin = rootStore.appStore.dofusTouchEarly ? DOFUS_EARLY_ORIGIN : DOFUS_ORIGIN
+    console.log(this._dofusOrigin)
     axiosRetry(this._httpClient, { retries: 5, retryDelay: () => 1000 })
   }
 
@@ -49,7 +50,7 @@ export class GameUpdater {
 
   async run() {
     // create folder if missing
-    // fs.rmSync(GAME_PATH, { recursive: true, force: true })
+    fs.rmSync(GAME_PATH, { recursive: true, force: true })
     fs.mkdirSync(GAME_PATH, { recursive: true })
     fs.mkdirSync(GAME_PATH + 'build', { recursive: true })
 
@@ -72,49 +73,55 @@ export class GameUpdater {
     })
 
     this._updaterWindow.sendProgress({ message: 'DOWNLOAD MISSING ASSETS FILES ON DISK..', percent: 10 })
-    await this._downloadAssetsFiles(assetDiffManifest, remoteAssetManifest).catch((error) => {
-      console.log('Error while downloading assets files:', error)
-      console.log('Will restart in non async mod')
-      this._downloadAssetsFiles(assetDiffManifest, remoteAssetManifest, false)
-    })
+    return this._downloadAssetsFiles(assetDiffManifest, remoteAssetManifest)
+      .catch((error) => {
+        console.log('Error while downloading assets files:', error)
+        console.log('Will restart in non async mod')
+        return this._downloadAssetsFiles(assetDiffManifest, remoteAssetManifest, false)
+      })
+      .then(async () => {
+        this._updaterWindow.sendProgress({ message: 'DOWNLOAD MISSING LINDO AND DOFUS FILES IN MEMORY..', percent: 40 })
+        const [missingLindoFiles, missingDofusFiles] = await this._retrieveMissingLindoAndDofusFiles(
+          lindoDiffManifest,
+          remoteLindoManifest,
+          dofusDiffManifest,
+          remoteDofusManifest
+        )
 
-    this._updaterWindow.sendProgress({ message: 'DOWNLOAD MISSING LINDO AND DOFUS FILES IN MEMORY..', percent: 40 })
-    const [missingLindoFiles, missingDofusFiles] = await this._retrieveMissingLindoAndDofusFiles(
-      lindoDiffManifest,
-      remoteLindoManifest,
-      dofusDiffManifest,
-      remoteDofusManifest
-    )
+        this._updaterWindow.sendProgress({ message: 'FINDING VERSIONS..', percent: 60 })
+        const localVersions = await this._findingVersions(missingDofusFiles)
 
-    this._updaterWindow.sendProgress({ message: 'FINDING VERSIONS..', percent: 60 })
-    const localVersions = await this._findingVersions(missingDofusFiles)
+        this._updaterWindow.sendProgress({
+          message: 'APPLYING REGEX (LINDO OVERRIDE) ON DOFUS MISSING FILES',
+          percent: 70
+        })
+        this._applyRegex(lindoDiffManifest, missingLindoFiles, missingDofusFiles)
 
-    this._updaterWindow.sendProgress({ message: 'APPLYING REGEX (LINDO OVERRIDE) ON DOFUS MISSING FILES', percent: 70 })
-    this._applyRegex(lindoDiffManifest, missingLindoFiles, missingDofusFiles)
+        this._updaterWindow.sendProgress({ message: 'WRITING LINDO AND DOFUS MISSING FILES TO DISK', percent: 80 })
+        this._writeMissingFiles(missingLindoFiles)
+        this._writeMissingFiles(missingDofusFiles)
 
-    this._updaterWindow.sendProgress({ message: 'WRITING LINDO AND DOFUS MISSING FILES TO DISK', percent: 80 })
-    this._writeMissingFiles(missingLindoFiles)
-    this._writeMissingFiles(missingDofusFiles)
+        this._updaterWindow.sendProgress({ message: 'REMOVING OLD ASSETS AND DOFUS FILES..', percent: 90 })
+        this._removeOldAssets(dofusDiffManifest, remoteDofusManifest)
+        this._removeOldAssets(lindoDiffManifest, remoteLindoManifest)
 
-    this._updaterWindow.sendProgress({ message: 'REMOVING OLD ASSETS AND DOFUS FILES..', percent: 90 })
-    this._removeOldAssets(dofusDiffManifest, remoteDofusManifest)
-    this._removeOldAssets(lindoDiffManifest, remoteLindoManifest)
+        this._updaterWindow.sendProgress({ message: 'SAVING ALL JSON FILES TO DISK', percent: 100 })
+        await Promise.all([
+          fs.promises.writeFile(LOCAL_ASSET_MAP_PATH, JSON.stringify(remoteAssetManifest)),
+          fs.promises.writeFile(LOCAL_LINDO_MANIFEST_PATH, JSON.stringify(remoteLindoManifest)),
+          fs.promises.writeFile(LOCAL_DOFUS_MANIFEST_PATH, JSON.stringify(remoteDofusManifest)),
+          fs.promises.writeFile(LOCAL_VERSIONS_PATH, JSON.stringify(localVersions))
+        ])
 
-    this._updaterWindow.sendProgress({ message: 'SAVING ALL JSON FILES TO DISK', percent: 100 })
-    await Promise.all([
-      fs.writeFile(LOCAL_ASSET_MAP_PATH, JSON.stringify(remoteAssetManifest)),
-      fs.writeFile(LOCAL_LINDO_MANIFEST_PATH, JSON.stringify(remoteLindoManifest)),
-      fs.writeFile(LOCAL_DOFUS_MANIFEST_PATH, JSON.stringify(remoteDofusManifest)),
-      fs.writeFile(LOCAL_VERSIONS_PATH, JSON.stringify(localVersions))
-    ])
-
-    // save to store
-    this._rootStore.appStore.setAppVersion(localVersions.appVersion)
-    this._rootStore.appStore.setBuildVersion(localVersions.buildVersion)
-    this._rootStore.appStore.setLindoVersion(app.getVersion())
-
-    console.log('UPDATE FINISH')
-    this._updaterWindow.close()
+        // save to store
+        this._rootStore.appStore.setAppVersion(localVersions.appVersion)
+        this._rootStore.appStore.setBuildVersion(localVersions.buildVersion)
+        this._rootStore.appStore.setLindoVersion(app.getVersion())
+      })
+      .finally(() => {
+        console.log('UPDATE FINISH')
+        this._updaterWindow.close()
+      })
   }
 
   private _writeMissingFiles(files: Files) {
@@ -217,73 +224,91 @@ export class GameUpdater {
     const dofusFiles: Files = {}
     for (const i in dofusDiff) {
       if (dofusDiff[i] === 1) {
-        dofusFiles[i] = await this._downloadFile(this._dofusOrigin + remoteDofus.files[i].filename)
+        dofusFiles[i] = await this._downloadFile(this._dofusOrigin + remoteDofus.files[i].filename).catch((err) => {
+          // switch on the normal version if it failed on the early version
+          if (this._dofusOrigin === DOFUS_EARLY_ORIGIN) {
+            return this._downloadFile(DOFUS_ORIGIN + remoteDofus.files[i].filename)
+          }
+          throw err
+        })
       }
     }
     return [lindoFiles, dofusFiles]
   }
 
-  private async _downloadAssetsFiles(diffManifest: DiffManifest, remoteAsset: Manifest, async: boolean = true) {
+  private async _downloadAssetsFiles(diffManifest: DiffManifest, remoteAsset: Manifest, parallel: boolean = true) {
     const initialStatus = 'Downloading Dofus files'
 
     const totalDownload = Object.keys(diffManifest).reduce((acc, key) => acc + (diffManifest[key] === 1 ? 1 : 0), 0)
     let currentDownload = 0
 
-    if (async) {
-      const promises = []
+    const promises = Object.keys(diffManifest)
+      .filter((key) => diffManifest[key] === 1)
+      .map(async (key) => {
+        const url = this._dofusOrigin + remoteAsset.files[key].filename
+        const filePath = GAME_PATH + remoteAsset.files[key].filename
 
-      for (const i in diffManifest) {
-        if (diffManifest[i] === 1) {
-          promises.push(
-            new Promise((resolve) => {
-              const url = this._dofusOrigin + remoteAsset.files[i].filename
-              const filePath = GAME_PATH + remoteAsset.files[i].filename
-
-              const directoryPath = path.dirname(filePath)
-              if (!fs.existsSync(directoryPath)) {
-                fs.mkdirSync(directoryPath, { recursive: true })
-              }
-
-              this._httpClient.get(url, { responseType: 'stream' }).then((response) => {
-                response.data.pipe(fs.createWriteStream(filePath))
-                currentDownload++
-
-                this._updaterWindow.sendProgress({
-                  message: initialStatus + ' (' + currentDownload + '/' + totalDownload + ')',
-                  percent: 10 + (currentDownload / totalDownload) * 30
-                })
-                resolve(true)
-              })
-            })
-          )
+        const directoryPath = path.dirname(filePath)
+        const fileExists = await fs.promises
+          .access(directoryPath, fs.constants.F_OK)
+          .then(() => true)
+          .catch(() => false)
+        if (!fileExists) {
+          await fs.promises.mkdir(directoryPath, { recursive: true })
         }
-      }
 
-      await Promise.all(promises)
-    } else {
-      for (const i in diffManifest) {
-        if (diffManifest[i] === 1) {
-          const url = this._dofusOrigin + remoteAsset.files[i].filename
-          const filePath = GAME_PATH + remoteAsset.files[i].filename
+        const fileWriteStream = fs.createWriteStream(filePath)
 
-          const directoryPath = path.dirname(filePath)
-          if (!fs.existsSync(directoryPath)) {
-            fs.mkdirSync(directoryPath, { recursive: true })
-          }
+        return this._httpClient
+          .get(url, { responseType: 'stream' })
+          .catch((err) => {
+            // switch on the normal version if it failed on the early version
+            if (this._dofusOrigin === DOFUS_EARLY_ORIGIN) {
+              return this._httpClient.get(DOFUS_ORIGIN + remoteAsset.files[key].filename, {
+                responseType: 'stream'
+              })
+            }
+            throw err
+          })
+          .then((response) => {
+            // response.data.pipe(fileWriteStream)
+            return new Promise<void>((resolve, reject) => {
+              const timeoutInt = setTimeout(() => {
+                reject(new Error('Timeout'))
+              }, 40000)
 
-          const fileWriteStream = fs.createWriteStream(filePath)
-
-          const response = await this._httpClient.get(url, { responseType: 'stream' })
-          response.data.pipe(fileWriteStream)
-
-          fileWriteStream.on('finish', () => {
+              fileWriteStream.on('finish', function () {
+                resolve()
+                clearTimeout(timeoutInt)
+              })
+              fileWriteStream.on('error', (err) => {
+                clearTimeout(timeoutInt)
+                reject(err)
+              })
+              response.data.pipe(fileWriteStream)
+            }).catch(() => {
+              console.log('Error while downloading ' + url)
+              console.log('This will ignore')
+            })
+          })
+          .then(() => {
+            currentDownload++
             this._updaterWindow.sendProgress({
               message: initialStatus + ' (' + currentDownload + '/' + totalDownload + ')',
               percent: 10 + (currentDownload / totalDownload) * 30
             })
-            currentDownload++
           })
+      })
+
+    if (parallel) {
+      await Promise.allSettled(promises).then((results) => {
+        if (results.some((result) => result.status === 'rejected')) {
+          throw new Error('Error while downloading files')
         }
+      })
+    } else {
+      for (const promise of promises) {
+        await promise
       }
     }
   }
